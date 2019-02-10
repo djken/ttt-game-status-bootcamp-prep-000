956b2ba91dc5f5c6804df5e6241edcc270fdd25c
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-02-10 16:39:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
     position_3 = board[win_index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
+      return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
